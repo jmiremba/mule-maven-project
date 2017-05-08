d64593c5eac3713cb3aa4023e745b8b525b69aec
Jubilee Miremba <jubilee.miremba@dcsdk12.org>
2017-05-08 14:08:48 -0600

    Initial commit of basic project.

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,19 @@
+package com.mulesoft.training;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+	@Override
+	protected String getConfigFile() {
+		// This should match the application .xml that has the flows to test in this class.
+		return "maven-project.xml";
+	}
+
+	@Test
+	public void mavenFlowDefaultResponse() throws Exception {
+		// Execute a flow and expect a response.
+		runFlowAndExpect("MavenFlow", "This is a simple Maven-configured Mule application.");
+	}
+}
